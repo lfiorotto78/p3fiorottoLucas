c48e3fd1092dfdc8740ef5c15814d0604e02e034
Lucas Fiorotto <lucasfiorottocode@gmail.com>
2024-04-11 19:02:55 -0300

    clase 11-4

--- example-app/app/Http/Controllers/StudentController.php
+++ example-app/app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
@@ -16,7 +17,26 @@ class StudentController extends Controller
         return view('student', ['students'=> $grupoA]);
     }
 
-    public function insert() {
-        dd('Hola');
+    public function insert(Request $request) {
+        $student = Student::create([
+            'dni' => $request->dni,
+            'nombre' => $request->nombre,
+            'apellido' => $request->apellido,
+            'fecha_nacimiento' => $request->nacimiento,
+        ]);
+
+        /*$student = new Student();
+        $student->name = $request->nombre;
+        $student->save();*/
+
+        //$student = Student::create($request->all());
+
+        return redirect('/alta');
+    }
+
+    public function delete($id) {
+        /*$students = Student::all();
+        $student = $students->find($id);
+        $student->delete();*/
     }
 }
