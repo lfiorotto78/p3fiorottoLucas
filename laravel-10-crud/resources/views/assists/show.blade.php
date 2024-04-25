663ce373b4b5050801bc915ea4dbcd48f6d2c45c
Lucas Fiorotto <lucasfiorottocode@gmail.com>
2024-04-25 19:21:19 -0300

    clase 25-4

---
+++ laravel-10-crud/resources/views/assists/show.blade.php
@@ -0,0 +1,31 @@
+@extends('students.layouts')
+
+@section('content')
+
+<div class="row justify-content-center mt-3">
+    <div class="col-md-8">
+
+        <div class="card">
+            <div class="card-header">
+                <div class="float-start">
+                    Student Assists
+                </div>
+                <div class="float-end">
+                    <a href="{{ route('students.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
+                </div>
+            </div>
+            <div class="card-body">
+                <table>
+                    <th>Fecha y Hora</th>
+                    @foreach ($assists as $assist)
+                    <tr>
+                        <td>{{ $assist->created_at }}</td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>    
+</div>
+    
+@endsection
