44721e72c9f82818de804c22af3697941d5c6ba1
Lucas Fiorotto <lucasfiorottocode@gmail.com>
2024-05-08 20:19:27 -0300

    clase 8-5

--- laravel-10-crud/app/Http/Controllers/AssistController.php
+++ laravel-10-crud/app/Http/Controllers/AssistController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Assist;
 use App\Models\Student;
+use Illuminate\Support\Facades\DB;
 
 class AssistController extends Controller
 {
@@ -17,5 +18,11 @@ class AssistController extends Controller
             'student' => $student,
             'assists' => $assists
         ]);
+    }
+    
+    public function store(Request $request)
+    {
+        $student = DB::table('students')->select('id')->where('dni', '=', $request->dni)->get();
+        DB::table('assists')->insert(['student_id' => $student[0]->id]);
     }
 }
