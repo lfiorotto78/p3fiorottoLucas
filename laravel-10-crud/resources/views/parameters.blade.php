1a2f28511b19ae370bbacd12624a0fa70a23029e
lfiorotto78 <lucasfiorottocode@gmail.com>
2024-06-06 15:20:46 -0300

    >estilo boton "Guardar" parametros

--- laravel-10-crud/resources/views/parameters.blade.php
+++ laravel-10-crud/resources/views/parameters.blade.php
@@ -4,15 +4,15 @@
             @csrf
 
             <label for="classes">Clases</label>
-            <input type="text" name="classes_quantity" id="classes" value="@isset($parameters->classes_quantity) {{ $parameters->classes_quantity }} @endisset">
+            <input type="text" name="classes_quantity" id="classes" value="@isset($parameters->classes_quantity) {{ $parameters->classes_quantity }} @endisset"> <br>
 
             <label for="promotion">Promoción</label>
-            <input type="text" name="promotion_percentage" id="promotion" value="@isset($parameters->promotion_percentage) {{ $parameters->promotion_percentage }} @endisset">
+            <input type="text" name="promotion_percentage" id="promotion" value="@isset($parameters->promotion_percentage) {{ $parameters->promotion_percentage }} @endisset"> <br>
 
             <label for="regular">Regular</label>
-            <input type="text" name="regular_percentage" id="regular" value="@isset($parameters->regular_percentage) {{ $parameters->regular_percentage }} @endisset">
+            <input type="text" name="regular_percentage" id="regular" value="@isset($parameters->regular_percentage) {{ $parameters->regular_percentage }} @endisset"> <br>
             
-            <input type="submit" value="Guardar">
+            <input type="submit" value="Guardar" class="px-2 py-1 rounded text-lg bg-blue-500 text-white">
         </form>
 
         @if (Session::has('success'))
